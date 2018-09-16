4a8393c4fa3eb7a9ffe81bb38175ab1dc2fbd3c9
uchihaitachi24 <manidharanupoju@gmail.com>
2018-09-16 17:04:14 +0530

    Jobs app up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    blogpost = models.CharField(max_length=1000)
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,2 @@
-from django.shortcuts import render
 
-
-def homepage(request):
-    return render(request, 'home.html')
 
